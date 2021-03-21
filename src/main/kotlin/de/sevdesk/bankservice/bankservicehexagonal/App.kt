7fc0aa663d44d43786b0589046e4dc0bb8aedb32
Simon Poloczek <Simon.Poloczek@sevdesk.de>
2021-03-21 20:21:27 +0100

    renamed kotlin app main class

--- src/main/kotlin/de/sevdesk/bankservice/bankservicehexagonal/App.kt
+++ src/main/kotlin/de/sevdesk/bankservice/bankservicehexagonal/App.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BankserviceHexagonalApplication
+class App
 
 fun main(args: Array<String>)
 {
-    runApplication<BankserviceHexagonalApplication>(*args)
+    runApplication<App>(*args)
 }
